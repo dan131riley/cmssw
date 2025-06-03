d9d107120e4588fbc20cc131d3bd8455bd1b4e42
Dan Riley <Daniel.Riley@cornell.edu>
2025-06-03 12:26:34 -0400

    enforce setting the tree for cache operations

--- IOPool/Input/src/InputFile.h
+++ IOPool/Input/src/InputFile.h
@@ -46,10 +46,8 @@ namespace edm {
     static void reportReadBranch(InputType inputType, std::string const& branchname);
 
     TObject* Get(char const* name) { return file_->Get(name); }
-    TFileCacheRead* GetCacheRead(const TObject* tree = nullptr) const { return file_->GetCacheRead(/* tree */); }
-    void SetCacheRead(TFileCacheRead* tfcr, TObject* tree = nullptr) {
-      file_->SetCacheRead(tfcr, nullptr /* tree */, TFile::kDoNotDisconnect);
-    }
+    TFileCacheRead* GetCacheRead(const TObject* tree) const { return file_->GetCacheRead(tree); }
+    void SetCacheRead(TFileCacheRead* tfcr, TObject* tree) { file_->SetCacheRead(tfcr, tree, TFile::kDoNotDisconnect); }
     void logFileAction(char const* msg, char const* fileName) const;
 
   private:
